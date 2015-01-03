752f3c048c0f0f5cc263ddaa0fd3e572c042679a
Salihan04 <salihan04@gmail.com>
2015-01-03 16:42:17 +0800

    Able to insert a document, update a particular document, delete the first document, query for a document from collection, and retrieve all documents

--- MongoDB Java/src/model/Person.java
+++ MongoDB Java/src/model/Person.java
@@ -1,14 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Person {
 	private String name;
 	private String gender;
 	private int age;
+	private static ArrayList<String> personParams = new ArrayList<String>();
+	private ArrayList<Object> paramValues = new ArrayList<Object>();	
 	
 	public Person(String name, String gender, int age) {
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
+		
+		personParams.add("name");
+		personParams.add("gender");
+		personParams.add("age");
+		
+		paramValues.add(this.name);
+		paramValues.add(this.gender);
+		paramValues.add(this.age);
 	}
 
 	public String getName() {
@@ -35,5 +47,15 @@ public class Person {
 		this.age = age;
 	}
 	
+	public static ArrayList<String> getPersonParams() {
+		return personParams;
+	}
 	
+	public static int getNumOfPersonParams() {
+		return personParams.size();
+	}
+	
+	public ArrayList<Object> getParamValues() {
+		return paramValues;
+	}
 }
