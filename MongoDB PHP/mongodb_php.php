bc840b1ae83ea2a06abafe39a3407ec8613755e7
Salihan04 <salihan04@gmail.com>
2015-01-04 11:40:05 +0800

    Setup MongoDB for PHP

---
+++ MongoDB PHP/mongodb_php.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MongoDB with PHP</title>
+</head>
+<body>
+	<header>
+		<h1>Using MongoDB with PHP</h1>
+		<div>
+			Includes:</br>
+			<ul>
+				<li>Connecting to database</li>
+				<li>Creating a collection</li>
+				<li>Inserting a document</li>
+				<li>Finding all documents</li>
+				<li>Updating a document</li>
+				<li>Deleting a document</li>
+			</ul>
+		</div>
+	</header>
+	</br>
+	<section>
+		<h2>Data</h2>
+		<article>
+			<table style="width: 30%">
+				<tr>
+					<th><u>Key</u></th>
+					<th><u>Value</u></th>
+				</tr>
+			</table>
+		<article>
+	</section>
+</body>
+</html>
